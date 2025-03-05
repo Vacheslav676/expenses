176f69ff89ba21ea7d2aca5cc971e8000c26d331
Вячеслав <115939591+Vacheslav676@users.noreply.github.com>
2025-03-05 10:30:05 +0300

    Update LocalDateConverter.kt перевожу коменты 3

--- app/src/main/java/com/nominalista/expenses/data/room/converter/LocalDateConverter.kt
+++ app/src/main/java/com/nominalista/expenses/data/room/converter/LocalDateConverter.kt
@@ -9,7 +9,9 @@ class LocalDateConverter {
 
     /**
      * First convert time since epoch to the ZonedDateTime. Epoch time is always in UTC.
+Сначала преобразуйте время, начиная с эпохи, в ZonedDateTime. Время эпохи всегда указывается в UTC.
      * Then convert ZonedDateTime to LocalDate.
+Затем преобразуйте ZonedDateTime в LocalDate.
      *
      * Example:
      * 946684800 -> 2000-01-01T00:00:00+00:00 -> 2000-01-01.
@@ -20,7 +22,9 @@ class LocalDateConverter {
 
     /**
      * First convert LocalDate to the ZonedDateTime. LocalDate doesn't hold zone information, so we
+Сначала преобразуйте LocalDate в ZonedDateTime. LocalDate не содержит информации о зоне, поэтому мы
      * can assume that the converted date is in UTC. Then convert ZonedDateTime to time since epoch.
+можно предположить, что преобразованная дата находится в UTC. Затем преобразуйте ZonedDateTime в время, прошедшее с эпохи.
      *
      * Example:
      * 2000-01-01 -> 2000-01-01T00:00:00+00:00 -> 946684800.
